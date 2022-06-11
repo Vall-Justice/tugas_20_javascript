4ac06f9ee816b25877dc27ad64795c8ce0b1cc38
Vall-Justice <104562832+Vall-Justice@users.noreply.github.com>
2022-06-11 14:13:41 +0700

    Tugas 20 Javascript

---
+++ tugas20.js
@@ -0,0 +1,5 @@
+let teks = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+
+console.log(teks.match("are"));
+console.log(teks.match("words"));
+
